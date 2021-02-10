caf7bb83926e9f4e0708805c1c11961ecca2a2eb
Schnellchen <delf97@mail.ru>
2021-02-10 13:51:24 +0300

    - destructuring assignment

--- src/service/giphy.service.ts
+++ src/service/giphy.service.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
 
-export const getTrending = function(): Promise<[object]> {
-  return axios
-    .get(
-      `${process.env.VUE_APP_BASE_URL}/trending?api_key=${process.env.VUE_APP_API_KEY}`
-    )
-    .then(result => result.data.data);
+export const getTrending = async function(): Promise<[object]> {
+  const {
+    data: { data }
+  } = await axios.get(
+    `${process.env.VUE_APP_BASE_URL}/trending?api_key=${process.env.VUE_APP_API_KEY}`
+  );
+  return data;
 };
 
-export const getSearched = function(query: string): Promise<[object]> {
-  return axios
-    .get(
-      `${process.env.VUE_APP_BASE_URL}/search?api_key=${process.env.VUE_APP_API_KEY}&q=${query}`
-    )
-    .then(result => result.data.data);
+export const getSearched = async function(query: string): Promise<[object]> {
+  const {
+    data: { data }
+  } = await axios.get(
+    `${process.env.VUE_APP_BASE_URL}/search?api_key=${process.env.VUE_APP_API_KEY}&q=${query}`
+  );
+  return data;
 };
